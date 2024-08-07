c89d2225fc7be748b2e1fe4f096fcfa3acad414b
murodullayevabduvali972@gmail.com <abduvalimurodullayev972@gmail.com>
2024-08-07 20:05:09 +0500

    Loyihani qayta yuklash

--- app/serializers/like.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from app.models import Like
-
-
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        fields = '__all__'
